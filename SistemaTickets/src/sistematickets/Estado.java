13607282e5aecedec6640192d791bf9e83badbca
Amada07 <amadacarcamo938@gmail.com>
2025-05-24 10:05:24 -0600

    Modificación de la clase Estado

--- SistemaTickets/src/sistematickets/Estado.java
+++ SistemaTickets/src/sistematickets/Estado.java
@@ -11,23 +11,38 @@ import java.util.ArrayList;
  * @author Amada
  */
 public class Estado {
+     private int idEstado;
     private String nombre;
     private String descripcion;
     private boolean estadoFinal;
-    private ArrayList<String> estadoPermitido;
+    private ArrayList<String> estadosPermitidos;
 
     // Constructor
-    public Estado(String nombre, String descripcion, boolean estadoFinal) {
+    public Estado(int idEstado,String nombre, String descripcion, boolean estadoFinal) {
         if (nombre == null || nombre.isEmpty() || nombre.length() < 3) {
             throw new IllegalArgumentException("El nombre del estado debe tener al menos 3 caracteres.");
         }
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.estadoFinal = estadoFinal;
-        this.estadoPermitido = new ArrayList<>();
+          this.estadosPermitidos = new ArrayList<>();
+    }
+    // Constructor alternativo SIN ID (para nuevos estados)
+    public Estado(String nombre, String descripcion, boolean estadoFinal) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.estadoFinal = estadoFinal;
+    }
+    
+    public int getIdEstado() {
+        return idEstado;
     }
 
     // Getters y Setters
+    public void setIdEstado(int idEstado) {    
+        this.idEstado = idEstado;
+    }
+
     public String getNombre() {
         return nombre;
     }
@@ -47,23 +62,23 @@ public class Estado {
         this.descripcion = descripcion;
     }
 
-    public boolean isestadoFinal() {
+    public boolean isEstadoFinal() {
         return estadoFinal;
     }
 
-    public void setestadoFinal(boolean estadoFinal) {
+    public void setEstadoFinal(boolean estadoFinal) {
         this.estadoFinal = estadoFinal;
         
     }
-    public ArrayList<String> getEstadoPermitido() {
-        return estadoPermitido;
+    public ArrayList<String> getEstadosPermitidos() {
+        return estadosPermitidos;
     }
 
-    public void agregarestadoPermitido(String estadoPermitido) {
+    public void agregarEstadoPermitido(String estadoPermitido) {
         if (estadoPermitido == null || estadoPermitido.isEmpty()) {
             throw new IllegalArgumentException("El técnico no puede estar vacío.");
         }
-        this.estadoPermitido.add(estadoPermitido);
+        this.estadosPermitidos.add(estadoPermitido);
     }
 }
     
